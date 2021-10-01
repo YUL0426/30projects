692a0a107d0d1d591781f6f5f25a5831ccfca9ec
YUL HUR <59783267+YUL0426@users.noreply.github.com>
2021-10-01 15:46:19 +0900

    Create 211001.swift

---
+++ 기본문법/211001.swift
@@ -0,0 +1,6 @@
+func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        self.tasks.remove(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .automatic)
+    }
+//삭제 버튼 누를시의 함수
+
