a411b480317e2838442616659642e8566cac454b
YUL HUR <59783267+YUL0426@users.noreply.github.com>
2021-10-01 12:57:40 +0900

    Create datasource.swift

---
+++ 기본문법/datasource.swift
@@ -0,0 +1,11 @@
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+
+  
+  UIViewtableDataSource활용시 위 두개의 메소드는 반드시 구현해 줘야 한다.
