43b99fac67db00e38fe9cc2212b74c4406ee15bf
YUL HUR <59783267+YUL0426@users.noreply.github.com>
2021-10-02 15:26:16 +0900

    Create Diary.swift

---
+++ 다이어리앱/Diary.swift
@@ -0,0 +1,17 @@
+//
+//  Diary.swift
+//  Diary
+//
+//  Created by YUL on 2021/10/02.
+//
+
+import Foundation
+
+struct Diary {
+    var title: String   
+    var contents : String
+    var date: Date
+    var isStar: Bool
+}
+//구조체 설정
+//왜와이?
