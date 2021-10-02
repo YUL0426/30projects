c2810879827e59512b14af33fac260b149eb5598
YUL HUR <59783267+YUL0426@users.noreply.github.com>
2021-10-02 15:17:37 +0900

    Create StarViewController.swift

---
+++ 다이어리앱/StarViewController.swift
@@ -0,0 +1,16 @@
+//
+//  StarViewController.swift
+//  Diary
+//
+//  Created by YUL on 2021/10/01.
+//
+
+import UIKit
+
+class StarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
