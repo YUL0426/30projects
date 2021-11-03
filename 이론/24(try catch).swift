f4fd1663208229ec7b13738ccf1d959e3b181144
YUL HUR <59783267+YUL0426@users.noreply.github.com>
2021-11-03 20:38:38 +0900

    Create 24(try catch).swift

---
+++ 이론/24(try catch).swift
@@ -0,0 +1,9 @@
+try catch : 프로그래 내에 에러가 발생한 상황에 대해 대응하고 이를 복구하느 과정
+swith의 에러 처리 : 발생(throwing), 감지(catching), 전파(propagating), 조작(manipulating)
+
+enum PhoneError : Error {
+  case unknown
+  case battery(batteryLevel: Int)
+}
+
+throw PhoneError.
