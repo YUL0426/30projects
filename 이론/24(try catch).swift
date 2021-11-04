f17a1bdce0d88f6a44b139986c47d20f6f3b7a1d
YUL HUR <59783267+YUL0426@users.noreply.github.com>
2021-11-04 19:48:42 +0900

    Update 24(try catch).swift

--- 이론/24(try catch).swift
+++ 이론/24(try catch).swift
@@ -8,3 +8,25 @@ enum PhoneError : Error {
 
 throw PhoneError.batteryLow(batteryLevel: 20)
 //오류가 떳을시 던져준다.
+
+ffunc checkPhoneBattteryStatus(batteryLevel: Int) throw -> String {
+  //스로잉 함수
+  //반환값이 있다면 함수 뒤에 작성
+  guard batteryLevel != -1 else { throw PhoneError.unknown }
+  //guard문 : false 일 때 else가 실행되며 조기종료됨
+  guard betteryLevel >= 20 else { throw PhoneError.batteryLow(batteryLevel: 20)}
+  
+  return "배터리 상태가 정상입니다."
+}
+
+/*
+//do catch문
+do {
+  try 오류 발생 가능 코드
+} catch 오류 패턴 {
+  처리코드
+}
+*/
+
+do {
+  try 
