94c935e4b1b155a7a855d22f98344e874c820a3e
YUL HUR <59783267+YUL0426@users.noreply.github.com>
2021-11-05 22:06:19 +0900

    Update coviddetailviewcontroller.swift

--- November,2021/211005/COVID19/coviddetailviewcontroller.swift
+++ November,2021/211005/COVID19/coviddetailviewcontroller.swift
@@ -17,10 +17,24 @@ class CovidDetailViewController: UITableViewController {
     @IBOutlet weak var overseasinflowCell: UITableViewCell!
     @IBOutlet weak var regionalOutbreakCell: UITableViewCell!
     
-    
+    //프로퍼티 선언
+    var covidOverview: CovidOverview? //옵셔널로 선언, 이프로퍼티로 선택된 지역의 발생현황 데이터를 전달 받을 것
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.configureView()
     }
-
+    
+    func configureView() {
+        guard let covidOverview = self.covidOverview else { return } // 옵셔널 바인딩
+        self.title = covidOverview.countryName //내비바에 지역이름을 위하여
+        self.newCaseCell.detailTextLabel?.text = "\(covidOverview.newCase)명"
+        self.totalCaseCell.detailTextLabel?.text = "\(covidOverview.totalCase)명"
+        self.recoveredCell.detailTextLabel?.text = "\(covidOverview.recovered)명"
+        self.deathCell.detailTextLabel?.text = "\(covidOverview.death)명"
+        self.percentageCell.detailTextLabel?.text = "\(covidOverview.percentage)%"
+        self.overseasinflowCell.detailTextLabel?.text = "\(covidOverview.newFcase)명"
+        self.regionalOutbreakCell.detailTextLabel?.text = "\(covidOverview.newCcase)명"
+        
+    }
 }
