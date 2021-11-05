752559cfb0a76aa680cb00747781f9e03e4a2798
YUL HUR <59783267+YUL0426@users.noreply.github.com>
2021-11-05 22:07:02 +0900

    Create citycovidoverview.swift

---
+++ November,2021/211005/COVID19/citycovidoverview.swift
@@ -0,0 +1,45 @@
+//
+//  CityCovidOverview.swift
+//  COVID19
+//
+//  Created by YUL on 2021/11/05.
+//
+
+import Foundation
+
+//매핑을 위한 구조체 정의
+struct CityCovidOverview: Codable {
+    let korea: CovidOverview
+    let seoul: CovidOverview
+    let busan: CovidOverview
+    let daegu: CovidOverview
+    let incheon: CovidOverview
+    let gwangju: CovidOverview
+    let daejeon: CovidOverview
+    let ulsan: CovidOverview
+    let sejong: CovidOverview
+    let gyeonggi: CovidOverview
+    let gangwon: CovidOverview
+    let chungbuk: CovidOverview
+    let chungnam: CovidOverview
+    let jeonbuk: CovidOverview
+    let jeonnam: CovidOverview
+    let gyeongbuk: CovidOverview
+    let gyeongnam: CovidOverview
+    let jeju: CovidOverview
+}
+
+
+//구조체 정의 및 코더블 채택
+struct CovidOverview: Codable {
+    let countryName: String
+    let newCase: String
+    let totalCase: String
+    let recovered: String
+    let death: String
+    let percentage: String
+    let newCcase: String
+    let newFcase: String
+}
+
+
