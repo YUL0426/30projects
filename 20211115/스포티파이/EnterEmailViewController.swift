34d3d1997a098b52bfa4fe3f2d8796c19c246216
YUL HUR <59783267+YUL0426@users.noreply.github.com>
2021-11-16 21:26:54 +0900

    Update EnterEmailViewController.swift

--- 20211115/스포티파이/EnterEmailViewController.swift
+++ 20211115/스포티파이/EnterEmailViewController.swift
@@ -6,12 +6,12 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class EnterEmailViewController: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var ErrorMessageLabel: UILabel!
-    
     @IBOutlet weak var nextButton: UIButton!
     
     override func viewDidLoad() {
@@ -37,6 +37,47 @@ class EnterEmailViewController: UIViewController {
 }
     
     @IBAction func nextButtonTapped(_ sender: UIButton) {
+        //Firebase 이메일/비밀번호 인증
+        let email = emailTextField.text ?? ""
+        let password = passwordTextField.text ?? ""
+        
+        //신규 사용자 생성
+        Auth.auth().createUser(withEmail: email, password: password) { [weak self] authResult, error in
+            guard let self = self else { return }
+            
+            if let error = error {
+                let code  = (error as NSError).code
+                switch code {
+                case 17007: //이미 가입한 계정일 때
+                    //로그인하기
+                    self.loginUser(withEmail: email, password: password)
+                default:
+                    self.ErrorMessageLabel.text = error.localizedDescription
+                }
+            } else {
+            self.showMainViewController() // 로그인이 제대로 됐을 시
+        }
+    }
+}
+    //코드 정상 실행 시 MVC 실행
+    private func showMainViewController() {
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        let mainViewController = storyboard.instantiateViewController(withIdentifier: "MainViewController")
+        mainViewController.modalPresentationStyle = .fullScreen
+        navigationController?.show(mainViewController, sender: nil)
+    }
+    
+    private func loginUser(withEmail email: String, password: String) {
+        Auth.auth().signIn(withEmail: email, password: password) { [weak self] _, error in
+            guard let self = self else { return }
+            
+            if let error = error {
+                self.ErrorMessageLabel.text = error.localizedDescription
+            } else {
+                self.showMainViewController()
+            }
+            
+        }
     }
 }
 
