64a0f4960154b4ad18af379345b7d9a0b3027636
YUL HUR <59783267+YUL0426@users.noreply.github.com>
2021-11-16 15:32:21 +0900

    Update MainController.swift

--- 20211115/스포티파이/MainController.swift
+++ 20211115/스포티파이/MainController.swift
@@ -1,1 +1,29 @@
 오늘은 스포티파이 앱을 통해 로그인하는 방법을 배울 것이다.
+
+//
+//  MainViewController.swift
+//  spotifyLoginSampleApp
+//
+//  Created by YUL on 2021/11/15.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    @IBOutlet weak var welcomeLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        //POP 제스쳐 방지
+        navigationController?.interactivePopGestureRecognizer?.isEnabled = false
+    }
+    //main에선 보여주지 않을 것
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.navigationBar.isHidden = true
+    }
+    
+    
+    @IBAction func logoutButtonTapped(_ sender: UIButton) {
+        self.navigationController?.popToRootViewController(animated: true)
+    }
+}
